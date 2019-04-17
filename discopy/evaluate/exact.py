cdec37c0b66f0ed6dbc618a15b8aef5c27ed7036
rknaebel <rknaebel@uni-potsdam.de>
2019-04-17 16:36:17 +0200

    remove confusion matrix implementation, get worse results

--- discopy/evaluate/exact.py
+++ discopy/evaluate/exact.py
@@ -1,224 +1,5 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-"""The Official CONLL 2016 Shared Task Scorer
-
-"""
-import json
-
 import numpy as np
-
-
-class ConfusionMatrix(object):
-    """Confusion matrix for evaluating a classifier
-    For more information on confusion matrix en.wikipedia.org/wiki/Confusion_matrix
-    """
-
-    INIT_NUM_CLASSES = 100
-    NEGATIVE_CLASS = '__NEGATIVE_CLASS__'
-
-    def __init__(self, alphabet=None):
-        if alphabet is None:
-            self.alphabet = Alphabet()
-            self.matrix = np.zeros((self.INIT_NUM_CLASSES, self.INIT_NUM_CLASSES))
-        else:
-            self.alphabet = alphabet
-            num_classes = alphabet.size()
-            self.matrix = np.zeros((num_classes, num_classes))
-
-    def __iadd__(self, other):
-        self.matrix += other.matrix
-        return self
-
-    def add(self, prediction, true_answer):
-        """Add one data point to the confusion matrix
-        If prediction is an integer, we assume that it's a legitimate index
-        on the confusion matrix.
-        If prediction is a string, then we will do the look up to
-        map to the integer index for the confusion matrix.
-        """
-        if type(prediction) == int and type(true_answer) == int:
-            self.matrix[prediction, true_answer] += 1
-        else:
-            self.alphabet.add(prediction)
-            self.alphabet.add(true_answer)
-            prediction_index = self.alphabet.get_index(prediction)
-            true_answer_index = self.alphabet.get_index(true_answer)
-            self.matrix[prediction_index, true_answer_index] += 1
-            # XXX: this will fail if the prediction_index is greater than
-            # the initial capacity. I should grow the matrix if this crashes
-
-    def add_list(self, predictions, true_answers):
-        """Add a list of data point to the confusion matrix
-        A list can be a list of integers.
-        If prediction is an integer, we assume that it's a legitimate index
-        on the confusion matrix.
-        A list can be a list of strings.
-        If prediction is a string, then we will do the look up to
-        map to the integer index for the confusion matrix.
-        """
-        for p, t in zip(predictions, true_answers):
-            self.add(p, t)
-
-    def get_prf_for_i(self, i):
-        """Compute precision, recall, and f1 score for a given index."""
-
-        if sum(self.matrix[i, :]) == 0:
-            precision = 1.0
-        else:
-            precision = self.matrix[i, i] / sum(self.matrix[i, :])
-        if sum(self.matrix[:, i]) == 0:
-            recall = 1.0
-        else:
-            recall = self.matrix[i, i] / sum(self.matrix[:, i])
-        if precision + recall != 0.0:
-            f1 = 2.0 * precision * recall / (precision + recall)
-        else:
-            f1 = 0.0
-        return (precision, recall, f1)
-
-    def get_prf_for_all(self):
-        """Compute precision, recall, and f1 score for all indexes."""
-
-        precision = np.zeros(self.alphabet.size())
-        recall = np.zeros(self.alphabet.size())
-        f1 = np.zeros(self.alphabet.size())
-
-        # compute precision, recall, and f1
-        for i in range(self.alphabet.size()):
-            precision[i], recall[i], f1[i] = self.get_prf_for_i(i)
-
-        return precision, recall, f1
-
-    def get_prf(self, class_name):
-        """Compute precision, recall, and f1 score for a given class. """
-        i = self.alphabet.get_index(class_name)
-        return self.get_prf_for_i(i)
-
-    def compute_average_f1(self):
-        precision, recall, f1 = self.get_prf_for_all()
-        return np.mean(f1)
-
-    def compute_average_prf(self):
-        precision, recall, f1 = self.get_prf_for_all()
-        return (round(np.mean(precision), 4),
-                round(np.mean(recall), 4),
-                round(np.mean(f1), 4))
-
-    def print_summary(self):
-
-        precision = np.zeros(self.alphabet.size())
-        recall = np.zeros(self.alphabet.size())
-        f1 = np.zeros(self.alphabet.size())
-
-        max_len = 0
-        for i in range(self.alphabet.size()):
-            label = self.alphabet.get_label(i)
-            if label != self.NEGATIVE_CLASS and len(label) > max_len:
-                max_len = len(label)
-
-        lines = []
-        correct = 0.0
-        # compute precision, recall, and f1
-        for i in range(self.alphabet.size()):
-            precision[i], recall[i], f1[i] = self.get_prf_for_i(i)
-            print(self.get_prf_for_i(i))
-            correct += self.matrix[i, i]
-            label = self.alphabet.get_label(i)
-            if label != self.NEGATIVE_CLASS:
-                space = ' ' * (max_len - len(label) + 1)
-                lines.append('%s%s precision %1.4f\trecall %1.4f\tF1 %1.4f' % \
-                             (label, space, precision[i], recall[i], f1[i]))
-        precision, recall, f1 = self.compute_micro_average_f1()
-        space = ' ' * (max_len - 14 + 1)
-        lines.append('*Micro-Average%s precision %1.4f\trecall %1.4f\tF1 %1.4f' % (
-            space, np.mean(precision), np.mean(recall), np.mean(f1)))
-        # lines.sort()
-        print('\n'.join(lines))
-
-
-class Alphabet(object):
-    """Two way map for label and label index
-    It is an essentially a code book for labels or features
-    This class makes it convenient for us to use numpy.array
-    instead of dictionary because it allows us to use index instead of
-    label string. The implemention of classifiers uses label index space
-    instead of label string space.
-    """
-
-    def __init__(self):
-        self._index_to_label = {}
-        self._label_to_index = {}
-        self.num_labels = 0
-        self.growing = True
-
-    def __len__(self):
-        return self.size()
-
-    def __eq__(self, other):
-        return self._index_to_label == other._index_to_label and \
-               self._label_to_index == other._label_to_index and \
-               self.num_labels == other.num_labels
-
-    def size(self):
-        return self.num_labels
-
-    def has_label(self, label):
-        return label in self._label_to_index
-
-    def get_label(self, index):
-        """Get label from index"""
-        if index >= self.num_labels:
-            raise KeyError("There are %d labels but the index is %d" % (self.num_labels, index))
-        return self._index_to_label[index]
-
-    def get_index(self, label):
-        """Get index from label"""
-        if not self.has_label(label):
-            if self.growing:
-                self.add(label)
-            else:
-                return -1
-        return self._label_to_index[label]
-
-    def add(self, label):
-        """Add an index for the label if it's a new label"""
-        if label not in self._label_to_index:
-            if not self.growing:
-                raise ValueError(
-                    'Alphabet is not set to grow i.e. accepting new labels')
-            self._label_to_index[label] = self.num_labels
-            self._index_to_label[self.num_labels] = label
-            self.num_labels += 1
-
-    def json_dumps(self):
-        return json.dumps(self.to_dict())
-
-    @classmethod
-    def json_loads(cls, json_string):
-        json_dict = json.loads(json_string)
-        return Alphabet.from_dict(json_dict)
-
-    def to_dict(self):
-        return {
-            '_label_to_index': self._label_to_index
-        }
-
-    @classmethod
-    def from_dict(cls, alphabet_dictionary):
-        """Create an Alphabet from dictionary
-        alphabet_dictionary is a dictionary with only one field
-        _label_to_index which is a map from label to index
-        and should be created with to_dict method above.
-        """
-        alphabet = cls()
-        alphabet._label_to_index = alphabet_dictionary['_label_to_index']
-        alphabet._index_to_label = {}
-        for label, index in alphabet._label_to_index.items():
-            alphabet._index_to_label[index] = label
-        # making sure that the dimension agrees
-        assert (len(alphabet._index_to_label) == len(alphabet._label_to_index))
-        alphabet.num_labels = len(alphabet._index_to_label)
-        return alphabet
+import sklearn
 
 
 def print_results(res_mean, mode):
@@ -245,22 +26,16 @@ def evaluate_all(gold_relations: dict, predicted_relations: dict):
 
         results.append(
             np.array([
-                connective_cm.get_prf('yes'),
-                arg1_cm.get_prf('yes'),
-                arg2_cm.get_prf('yes'),
-                rel_arg_cm.get_prf('yes'),
-                sense_cm.compute_average_prf(),
+                connective_cm,
+                arg1_cm,
+                arg2_cm,
+                rel_arg_cm,
+                sense_cm,
             ])
         )
-        print(sense_cm.matrix)
-        print(sense_cm.alphabet._index_to_label)
-        print(sense_cm.alphabet._label_to_index)
-        break
 
     results = np.stack(results)
-    print(results[0])
     res_mean, res_std = results.mean(0), results.std(0)
-    print(res_mean)
     print_results(res_mean, 'ALL')
 
     results = []
@@ -272,16 +47,13 @@ def evaluate_all(gold_relations: dict, predicted_relations: dict):
         arg1_cm, arg2_cm, rel_arg_cm = evaluate_argument_extractor(gold_list, predicted_list)
         sense_cm = evaluate_sense(gold_list, predicted_list)
 
-        if sense_cm.compute_average_prf()[1] > 1:
-            sense_cm.print_summary()
-
         results.append(
             np.array([
-                connective_cm.get_prf('yes'),
-                arg1_cm.get_prf('yes'),
-                arg2_cm.get_prf('yes'),
-                rel_arg_cm.get_prf('yes'),
-                sense_cm.compute_average_prf(),
+                connective_cm,
+                arg1_cm,
+                arg2_cm,
+                rel_arg_cm,
+                sense_cm,
             ])
         )
 
@@ -298,16 +70,13 @@ def evaluate_all(gold_relations: dict, predicted_relations: dict):
         arg1_cm, arg2_cm, rel_arg_cm = evaluate_argument_extractor(gold_list, predicted_list)
         sense_cm = evaluate_sense(gold_list, predicted_list)
 
-        if sense_cm.compute_average_prf()[1] > 1:
-            sense_cm.print_summary()
-
         results.append(
             np.array([
-                connective_cm.get_prf('yes'),
-                arg1_cm.get_prf('yes'),
-                arg2_cm.get_prf('yes'),
-                rel_arg_cm.get_prf('yes'),
-                sense_cm.compute_average_prf(),
+                connective_cm,
+                arg1_cm,
+                arg2_cm,
+                rel_arg_cm,
+                sense_cm,
             ])
         )
 
@@ -419,63 +188,66 @@ def evaluate_sense(gold_list, predicted_list):
     that are missed by the system
     because the arguments don't match any of the gold relations.
     """
-    sense_alphabet = Alphabet()
-    for relation in gold_list:
-        sense = relation.senses[0]
-        sense_alphabet.add(sense)
-
-    sense_alphabet.add(ConfusionMatrix.NEGATIVE_CLASS)
-
-    sense_cm = ConfusionMatrix(sense_alphabet)
-    gold_to_predicted_map, predicted_to_gold_map = _link_gold_predicted(gold_list, predicted_list)
-
-    for i, gold_relation in enumerate(gold_list):
-        gold_sense = gold_relation.senses[0]
-        if i in gold_to_predicted_map:
-            predicted_sense = predicted_list[gold_to_predicted_map[i]].senses[0]
-            if predicted_sense in gold_relation.senses:
-                sense_cm.add(predicted_sense, predicted_sense)
+    y_true, y_pred = [], []
+    gold_to_predicted_map, _ = _link_gold_predicted(gold_list, predicted_list)
+    for gi, gr in enumerate(gold_list):
+        if gi in gold_to_predicted_map:
+            senses = [s for s in predicted_list[gold_to_predicted_map[gi]].senses if s in gr.senses]
+            if senses:
+                y_true.append(senses[0])
+                y_pred.append(senses[0])
             else:
-                if not sense_cm.alphabet.has_label(predicted_sense):
-                    predicted_sense = ConfusionMatrix.NEGATIVE_CLASS
-                sense_cm.add(predicted_sense, gold_sense)
+                y_true.append(gr.senses[0])
+                y_pred.append(predicted_list[gold_to_predicted_map[gi]].senses[0])
         else:
-            sense_cm.add(ConfusionMatrix.NEGATIVE_CLASS, gold_sense)
+            y_true.append(gr.senses[0])
+            y_pred.append('NO_MATCH')
 
-    for i, predicted_relation in enumerate(predicted_list):
-        if i not in predicted_to_gold_map:
-            predicted_sense = predicted_relation.senses[0]
-            if not sense_cm.alphabet.has_label(predicted_sense):
-                print(predicted_sense, 'not in alphabet')
-                predicted_sense = ConfusionMatrix.NEGATIVE_CLASS
-            sense_cm.add(predicted_sense, ConfusionMatrix.NEGATIVE_CLASS)
-    return sense_cm
+    if not y_true:
+        return 0.0, 0.0, 0.0
+    else:
+        precision, recall, f1, support = sklearn.metrics.precision_recall_fscore_support(np.array(y_true),
+                                                                                         np.array(y_pred),
+                                                                                         average='macro')
+    return precision, recall, f1
 
 
 def compute_binary_eval_metric(gold_list, predicted_list, matching_fn):
     """Compute binary evaluation metric
 
     """
-    binary_alphabet = Alphabet()
-    binary_alphabet.add('yes')
-    binary_alphabet.add('no')
-    cm = ConfusionMatrix(binary_alphabet)
+    tp = fp = fn = 0
     matched_predicted = [False for _ in predicted_list]
     for gold_span in gold_list:
         found_match = False
         for i, predicted_span in enumerate(predicted_list):
             if matching_fn(gold_span, predicted_span) and not matched_predicted[i]:
-                cm.add('yes', 'yes')
+                tp += 1
                 matched_predicted[i] = True
                 found_match = True
                 break
         if not found_match:
-            cm.add('no', 'yes')
+            fp += 1
     # Predicted span that does not match with any
     for matched in matched_predicted:
         if not matched:
-            cm.add('yes', 'no')
-    return cm
+            fn += 1
+
+    if tp + fp == 0:
+        precision = 0.0
+    else:
+        precision = tp / (tp + fp)
+    if tp + fn == 0:
+        recall = 1.0
+    else:
+        recall = tp / (tp + fn)
+
+    if precision + recall > 0:
+        f1 = (2 * precision * recall) / (precision + recall)
+    else:
+        f1 = 0
+
+    return precision, recall, f1
 
 
 def _link_gold_predicted(gold_list, predicted_list):
@@ -494,7 +266,7 @@ def _link_gold_predicted(gold_list, predicted_list):
 
     for gi, gr in enumerate(gold_list):
         for pi, pr in enumerate(predicted_list):
-            if span_exact_matching(gr.arg1|gr.arg2, pr.arg1|pr.arg2):
+            if span_exact_matching(gr.arg1 | gr.arg2, pr.arg1 | pr.arg2):
                 gold_to_predicted_map[gi] = pi
                 predicted_to_gold_map[pi] = gi
     return gold_to_predicted_map, predicted_to_gold_map
