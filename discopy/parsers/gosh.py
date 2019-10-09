132a091da85c776f4f6399a0d1dae25927901f48
rknaebel <rknaebel@uni-potsdam.de>
2019-10-09 10:47:20 +0200

    add simpler gosh parser based on lin pipeline

--- discopy/parsers/gosh.py
+++ discopy/parsers/gosh.py
@@ -1,16 +1,10 @@
-import copy
 import logging
-import os
 
-import joblib
 import nltk
-import numpy as np
 
 from discopy.labeling.extract.gosh_arg_extract import GoshArgumentExtract
-from discopy.labeling.connective import ConnectiveClassifier
-from discopy.parsers.utils import get_raw_tokens
-from discopy.sense.explicit import ExplicitSenseClassifier
-from discopy.sense.nonexplicit import NonExplicitSenseClassifier
+from discopy.parsers.lin import LinParser
+from discopy.parsers.utils import get_token_list2, get_raw_tokens2
 
 logger = logging.getLogger('discopy')
 
@@ -21,186 +15,41 @@ def get_token_list(doc_words, tokens, sent_id, sent_off):
              sent_off + t, sent_id, t] for t in tokens]
 
 
-class GoshParser(object):
+class GoshParser(LinParser):
 
-    def __init__(self, n_estimators=1):
-        self.connective_clf = ConnectiveClassifier(n_estimators=n_estimators)
+    def __init__(self):
+        super().__init__()
         self.arg_extract_clf = GoshArgumentExtract()
-        self.explicit_clf = ExplicitSenseClassifier(n_estimators=n_estimators)
-        self.non_explicit_clf = NonExplicitSenseClassifier(n_estimators=n_estimators)
 
-    def train(self, pdtb, parses, pdtb_val=None, parses_val=None):
-        logger.info('Train Connective Classifier...')
-        self.connective_clf.fit(pdtb, parses)
-        logger.info('Train Argument Extractor...')
-        self.arg_extract_clf.fit(pdtb, parses)
-        logger.info('Train Explicit Sense Classifier...')
-        self.explicit_clf.fit(pdtb, parses)
-        logger.info('Train Non-Explicit Sense Classifier...')
-        self.non_explicit_clf.fit(pdtb, parses)
+    def parse_explicit_arguments(self, doc, relations):
+        doc_words = [(w, s_i, w_i) for s_i, s in enumerate(doc['sentences']) for w_i, w in enumerate(s['words'])]
 
-    def score(self, pdtb, parses):
-        self.connective_clf.score(pdtb, parses)
-        self.arg_extract_clf.score(pdtb, parses)
-        self.explicit_clf.score(pdtb, parses)
-        self.non_explicit_clf.score(pdtb, parses)
-
-    def save(self, path):
-        if not os.path.exists(path):
-            os.makedirs(path)
-
-        self.connective_clf.save(path)
-        self.arg_extract_clf.save(path)
-        self.explicit_clf.save(path)
-        self.non_explicit_clf.save(path)
-        joblib.dump(self, os.path.join(path, 'parser.joblib'))
-
-    def load(self, path, parses=None):
-        if not os.path.exists(path):
-            raise FileNotFoundError('Path not found')
-        self.connective_clf.load(path)
-        self.arg_extract_clf.load(path)
-        self.explicit_clf.load(path)
-        self.non_explicit_clf.load(path)
-
-    @staticmethod
-    def from_path(path):
-        return joblib.load(os.path.join(path, 'parser.joblib'))
-
-    def parse_documents(self, documents):
-        relations = []
-        for idx, (doc_id, doc) in enumerate(documents.items()):
-            parsed_relations = self.parse_doc(doc)
-            for p in parsed_relations:
-                p['DocID'] = doc_id
-            relations.extend(parsed_relations)
-
-        return relations
-
-    def parse_doc(self, doc):
-        output = []
-        token_id = 0
-        sent_offset = 0
-        inter_relations = set()
-        doc_words = []
-        for s_i, s in enumerate(doc['sentences']):
-            for w_i, w in enumerate(s['words']):
-                word = copy.deepcopy(w)
-                word['sent_id'] = s_i
-                word['word_id'] = w_i
-                doc_words.append(word)
-
-        for sent_id, sent in enumerate(doc['sentences']):
-            sent_len = len(sent['words'])
+        for relation in relations:
+            sent_id = relation.Connective.get_sentence_ids()[0]
             try:
-                ptree = nltk.ParentedTree.fromstring(sent['parsetree'])
+                ptree = nltk.ParentedTree.fromstring(doc['sentences'][sent_id]['parsetree'])
             except ValueError:
                 logger.warning('Failed to parse doc {} idx {}'.format(doc['DocID'], sent_id))
-                token_id += sent_len
                 continue
             if not ptree.leaves():
                 logger.warning('Failed on empty tree')
-                token_id += sent_len
                 continue
-            current_token = 0
-            while current_token < sent_len:
-                relation = {
-                    'Connective': {},
-                    'Arg1': {},
-                    'Arg2': {},
-                    'Type': 'Explicit',
-                    'Confidences': {}
-                }
+            arg1, arg2, arg1_c, arg2_c = self.arg_extract_clf.extract_arguments(doc, relation.to_dict())
 
-                # CONNECTIVE CLASSIFIER
-                connective, connective_confidence = self.connective_clf.get_connective(ptree, sent['words'],
-                                                                                       current_token)
-                # whenever a position is not identified as connective, go to the next token
-                if not connective:
-                    token_id += 1
-                    current_token += 1
-                    continue
+            relation.Arg1.TokenList = get_token_list2(doc_words, arg1)
+            relation.Arg1.RawText = get_raw_tokens2(doc_words, arg1)
+            relation.Arg2.TokenList = get_token_list2(doc_words, arg2)
+            relation.Arg2.RawText = get_raw_tokens2(doc_words, arg2)
+        return [r for r in relations if r.is_valid()]
 
-                relation['Connective']['TokenList'] = get_token_list(doc_words, [current_token + t_sent for t_sent in
-                                                                                 range(len(connective))], sent_id,
-                                                                     sent_offset)
-                relation['Connective']['RawText'] = get_raw_tokens(doc_words, relation['Connective']['TokenList'])
-                relation['Confidences']['Connective'] = connective_confidence
-
-                leaf_index = [i[4] for i in relation['Connective']['TokenList']]
-                # arg_pos, arg_pos_confidence = self.arg_pos_clf.get_argument_position(ptree, ' '.join(connective),
-                #                                                                      leaf_index)
-
-                # ARGUMENT EXTRACTION
-                sent_prev = doc['sentences'][sent_id - 1]
-                arg1, arg2, arg1_c, arg2_c = self.arg_extract_clf.extract_arguments(ptree, relation)
-                len_prev = len(sent_prev['words'])
-                relation['Arg1']['TokenList'] = get_token_list(doc_words, arg1, sent_id, sent_offset)
-                relation['Arg2']['TokenList'] = get_token_list(doc_words, arg2, sent_id, sent_offset)
-                relation['Arg1']['RawText'] = get_raw_tokens(doc_words, relation['Arg1']['TokenList'])
-                relation['Arg2']['RawText'] = get_raw_tokens(doc_words, relation['Arg2']['TokenList'])
-                relation['Confidences']['Arg1'] = arg1_c
-                relation['Confidences']['Arg2'] = arg2_c
-
-                # TODO check whether explicit span is over adjacent sentences
-                inter_relations.add(sent_id)
-
-                # EXPLICIT SENSE
-                explicit, explicit_c = self.explicit_clf.get_sense(relation, ptree)
-                relation['Sense'] = [explicit]
-                relation['Confidences']['Sense'] = explicit_c
-                output.append(relation)
-                token_id += len(connective)
-                current_token += len(connective)
-            sent_offset += sent_len
-
-        token_id = 0
-        sent_lengths = [0]
-        for sent_id, sent in enumerate(doc['sentences']):
-            sent_lengths.append(len(sent['words']))
-            if sent_id == 0 or sent_id in inter_relations:
-                token_id += len(sent['words'])
-                continue
-
-            try:
-                ptree = nltk.ParentedTree.fromstring(sent['parsetree'])
-                ptree_prev = nltk.ParentedTree.fromstring(doc['sentences'][sent_id - 1]['parsetree'])
-                dtree = sent['dependencies']
-                dtree_prev = doc['sentences'][sent_id - 1]['dependencies']
-            except ValueError:
-                logger.warning('Failed to parse doc {} idx {}'.format(doc['DocID'], sent_id))
-                continue
-
-            if not ptree.leaves() or not ptree_prev.leaves():
-                continue
-
-            relation = {
-                'Connective': {
-                    'TokenList': []
-                },
-                'Arg1': {
-                    'TokenList': get_token_list(doc_words, list(range(len(ptree_prev.leaves()))), sent_id - 1,
-                                                sum(sent_lengths[:-2]))
-                },
-                'Arg2': {
-                    'TokenList': get_token_list(doc_words, list(range(len(ptree.leaves()))), sent_id,
-                                                sum(sent_lengths[:-1]))
-                },
-                'Type': 'Implicit',
-                'Sense': [],
-                'Confidences': {}
-            }
-            relation['Arg1']['RawText'] = get_raw_tokens(doc_words, relation['Arg1']['TokenList'])
-            relation['Arg2']['RawText'] = get_raw_tokens(doc_words, relation['Arg2']['TokenList'])
-            arg1 = [doc_words[i[2]][0] for i in relation['Arg1']['TokenList']]
-            arg2 = [doc_words[i[2]][0] for i in relation['Arg2']['TokenList']]
-            sense, sense_c = self.non_explicit_clf.get_sense(ptree_prev, ptree, dtree_prev, dtree, arg1, arg2)
-            relation['Sense'] = [sense]
-            relation['Confidences']['Sense'] = sense_c
-            output.append(relation)
-
-            token_id += len(sent['words'])
-
-        for r in output:
-            r['Confidence'] = np.mean(list(r['Confidences'].values()))
-        return output
+    def parse_doc(self, doc):
+        relations = self.parse_connectives(doc)
+        relations = self.parse_explicit_sense(doc, relations)
+        relations = self.parse_explicit_arguments(doc, relations)
+        relations = self.parse_implicit_arguments(doc, relations)
+        relations = self.parse_implicit_senses(doc, relations)
+        # TODO remove later
+        # transforms the relation structure into dict format
+        # just for now as long as the relation structure is used locally only
+        relations = [r.to_conll() for r in relations]
+        return relations
