08145fadc00b55dbeb6a74012bd4ce8ea47aaeef
rknaebel <rknaebel@uni-potsdam.de>
2020-11-25 14:45:23 +0100

    update bert embedding generation script

--- examples/make_bert_embeddings.py
+++ examples/make_bert_embeddings.py
@@ -1,7 +1,8 @@
-import argparse
 import os
 
+import click
 import joblib
+from spacy_transformers import TransformersLanguage, TransformersWordPiecer, TransformersTok2Vec
 
 from discopy.data.conll16 import get_conll_dataset
 
@@ -10,17 +11,6 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 from tqdm import tqdm
 
 from discopy.utils import init_logger
-
-import spacy
-
-argument_parser = argparse.ArgumentParser()
-argument_parser.add_argument("--model", help="",
-                             default='en_trf_bertbaseuncased_lg')
-argument_parser.add_argument("--conll", help="",
-                             default='')
-args = argument_parser.parse_args()
-
-os.makedirs(args.dir, exist_ok=True)
 
 logger = init_logger()
 
@@ -37,7 +27,7 @@ def get_doc_sentences(parses):
             parses.items()}
 
 
-def transform_to_bert_emebddings(nlp, parses, use_all_embeddings=False):
+def transform_to_bert_emebddings(nlp, parses):
     parses_bert = {}
     sentences = get_doc_sentences(parses)
     for doc_id, doc in tqdm(parses.items()):
@@ -48,24 +38,36 @@ def transform_to_bert_emebddings(nlp, parses, use_all_embeddings=False):
     return parses_bert
 
 
-def main():
-    nlp = spacy.load(args.model)
+@click.command()
+@click.argument('model', type=str)
+@click.argument('model-name', type=str)
+@click.argument('conll-path', type=str)
+@click.argument('lang', type=str)
+def main(model, model_name, conll_path, lang):
+    nlp = TransformersLanguage(trf_name=model, meta={"lang": lang})
+    nlp.add_pipe(nlp.create_pipe("sentencizer"))
+    nlp.add_pipe(TransformersWordPiecer.from_pretrained(nlp.vocab, model))
+    nlp.add_pipe(TransformersTok2Vec.from_pretrained(nlp.vocab, model))
     nlp.tokenizer = nlp.tokenizer.tokens_from_list
 
-    parses_train, pdtb_train = get_conll_dataset(args.conll, 'en.train', load_trees=False, connective_mapping=True)
-    parses_val, pdtb_val = get_conll_dataset(args.conll, 'en.dev', load_trees=False, connective_mapping=True)
-    parses_test, pdtb_test = get_conll_dataset(args.conll, 'en.test', load_trees=False, connective_mapping=True)
-    parses_blind, pdtb_blind = get_conll_dataset(args.conll, 'en.blind-test', load_trees=False, connective_mapping=True)
+    parses_train, pdtb_train = get_conll_dataset(conll_path, 'en.train', load_trees=False, connective_mapping=True)
+    parses_val, pdtb_val = get_conll_dataset(conll_path, 'en.dev', load_trees=False, connective_mapping=True)
+    parses_test, pdtb_test = get_conll_dataset(conll_path, 'en.test', load_trees=False, connective_mapping=True)
+    parses_blind, pdtb_blind = get_conll_dataset(conll_path, 'en.blind-test', load_trees=False, connective_mapping=True)
+    parses_pcc, pdtb_pcc = get_conll_dataset(conll_path, 'de.pcc', load_trees=False, connective_mapping=False,
+                                             types=None)
 
     bert_train = transform_to_bert_emebddings(nlp, parses_train)
     bert_val = transform_to_bert_emebddings(nlp, parses_val)
     bert_test = transform_to_bert_emebddings(nlp, parses_test)
     bert_blind = transform_to_bert_emebddings(nlp, parses_blind)
+    bert_pcc = transform_to_bert_emebddings(nlp, parses_pcc)
 
-    joblib.dump(bert_train, os.path.join(args.conll, 'en.train', 'bert.joblib'))
-    joblib.dump(bert_val, os.path.join(args.conll, 'en.dev', 'bert.joblib'))
-    joblib.dump(bert_test, os.path.join(args.conll, 'en.test', 'bert.joblib'))
-    joblib.dump(bert_blind, os.path.join(args.conll, 'en.blind-test', 'bert.joblib'))
+    joblib.dump(bert_train, os.path.join(conll_path, 'en.train', '{}.joblib'.format(model_name)))
+    joblib.dump(bert_val, os.path.join(conll_path, 'en.dev', '{}.joblib'.format(model_name)))
+    joblib.dump(bert_test, os.path.join(conll_path, 'en.test', '{}.joblib'.format(model_name)))
+    joblib.dump(bert_blind, os.path.join(conll_path, 'en.blind-test', '{}.joblib'.format(model_name)))
+    joblib.dump(bert_pcc, os.path.join(conll_path, 'de.pcc', '{}.joblib'.format(model_name)))
 
 
 if __name__ == '__main__':
